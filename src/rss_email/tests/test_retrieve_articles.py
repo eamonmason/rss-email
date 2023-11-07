50c2e99d4867c513463ef7761fcf5e8b18f0eb0e
Eamon Mason <eamon.mason@thomsonreuters.com>
2023-11-07 14:37:57 +0000

    More unit tests

--- src/rss_email/tests/test_retrieve_articles.py
+++ src/rss_email/tests/test_retrieve_articles.py
@@ -1,6 +1,7 @@
 """Tests for the retrieve_articles module."""
 import os
-from unittest.mock import MagicMock
+from unittest.mock import MagicMock, patch
+
 
 import boto3
 from moto import mock_s3
@@ -35,6 +36,32 @@ def test_create_rss():
 def test_get_feed_urls():
     """Tests that the feed URLs are returned correctly."""
     feed_urls = get_feed_urls(os.path.join(os.path.dirname(__file__), 'test_urls.json'))
+    assert len(feed_urls) == 2
+    assert feed_urls[0] == 'https://foo.com/feed/'
+    assert feed_urls[1] == 'https://bar.com/posts.atom'
+
+@patch('rss_email.retrieve_articles.files')
+def test_get_feed_urls2(mock_file):
+    """Tests that the feed URLs are returned correctly."""
+    mock_file_obj = MagicMock()
+    mock_file_obj.read_text.return_value = '''{
+        "feeds": [
+            {
+                "name": "Test Feed A",
+                "url": "https://foo.com/feed/"
+            },
+            {
+                "name": "Test Feed B",
+                "url": "https://bar.com/posts.atom"
+            },
+            {
+                "name": "Test Feed C",
+                "_url": "https://acme.com/feed.xml"
+            }
+        ]
+    }'''
+    mock_file.return_value.joinpath.return_value = mock_file_obj
+    feed_urls = get_feed_urls('dummyfile.json')
     assert len(feed_urls) == 2
     assert feed_urls[0] == 'https://foo.com/feed/'
     assert feed_urls[1] == 'https://bar.com/posts.atom'
